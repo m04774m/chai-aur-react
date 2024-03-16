8749cc09c296481ebfb8354b31671fce0307d782
Moazzam Majid <66308665+m04774m@users.noreply.github.com>
2024-03-17 01:16:48 +0530

    Update AllPosts.jsx

--- 12MegaBlog/src/pages/AllPosts.jsx
+++ 12MegaBlog/src/pages/AllPosts.jsx
@@ -1,15 +1,17 @@
 import React, {useState, useEffect} from 'react'
 import { Container, PostCard } from '../components'
-import appwriteService from "../appwrite/config";
+import service from "../appwrite/config"; //service was imported instead of appwriteService from config
 
 function AllPosts() {
     const [posts, setPosts] = useState([])
-    useEffect(() => {}, [])
-    appwriteService.getPosts([]).then((posts) => {
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
         if (posts) {
             setPosts(posts.documents)
-        }
+        } //this code was put inside the useEffect scope previously it was not
     })
+    }, [])
+    
   return (
     <div className='w-full py-8'>
         <Container>
